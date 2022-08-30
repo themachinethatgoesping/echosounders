c009f7f5718b8da02de31b6598d01b4b8ec60bd1
Peter Urban <peter.urban@ugent.be>
2022-08-30 16:09:49 +0200

    added __version__ attribute

--- src/pymodule/module.cpp
+++ src/pymodule/module.cpp
@@ -11,11 +11,12 @@
 // declare modules
 //void init_m_navtools(pybind11::module& m);                     // m_navtools.cpp
 
-PYBIND11_MODULE(PYTHON_LIB, m)
+PYBIND11_MODULE(MODULE_NAME, m)
 {
     pybind11::add_ostream_redirect(m, "ostream_redirect");
 
     m.doc() = "Python module to read, write and process single- and multibeam echosounder data formats";
+    m.attr("__version__") = MODULE_VERSION;
 
     //init_m_navtools(m);
     init_m_simrad(m);
