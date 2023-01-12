578f3983feb734e4b4891035bdbcbbbeefe2abad
Peter Urban <peter.urban@ugent.be>
2023-01-12 10:59:02 +0100

    towards seabedimagedata datagram (no python module yet)

---
+++ src/themachinethatgoesping/echosounders/em3000/datagrams/seabedimagedata.hpp
@@ -0,0 +1,302 @@
+// SPDX-FileCopyrightText: 2022 Peter Urban, GEOMAR Helmholtz Centre for Ocean Research Kiel
+// SPDX-FileCopyrightText: 2022 Sven Schorge, GEOMAR Helmholtz Centre for Ocean Research Kiel
+// SPDX-FileCopyrightText: 2022 - 2023 Peter Urban, Ghent University
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+
+/* generated doc strings */
+#include ".docstrings/seabedimagedata.doc.hpp"
+
+// std includes
+#include <string>
+#include <vector>
+
+// themachinethatgoesping import
+#include <themachinethatgoesping/tools/classhelper/objectprinter.hpp>
+#include <themachinethatgoesping/tools/classhelper/stream.hpp>
+#include <themachinethatgoesping/tools/timeconv.hpp>
+
+#include "../em3000_types.hpp"
+#include "em3000datagram.hpp"
+
+#include "substructures/seabedimagedatabeam.hpp"
+
+namespace themachinethatgoesping {
+namespace echosounders {
+namespace em3000 {
+namespace datagrams {
+
+/**
+ * @brief This datagram is used for the models EM 2040, EM 2040C, EM 710, EM 122, EM
+302 and ME70BO. All receiver beams are included, check detection info and real time
+cleaning for beam status (note 4 and 5).
+ */
+class SeabedImageData : public EM3000Datagram
+{
+  protected:
+    uint16_t _ping_counter; // sequential number
+    uint16_t _system_serial_number;
+    float    _sampling_frequency;           // in Hz
+    uint16_t _range_to_normal_incidence;    // used to correct sample amplitudes in no. of samples
+    int16_t  _normal_incidence_backscatter; // in 0.01 dB (BSN)
+    int16_t  _oblique_backscatter;          // in 0.01 dB (BSO)
+    uint16_t _tx_beamwidth_along;           // in 0.1 degree
+    uint16_t _tvg_law_crossover_angle;      // in 0.1 degree
+    uint16_t _number_of_valid_beams;
+
+    std::vector<substructures::SeabedImageDataBeam> _beams;
+
+    std::vector<std::vector<int16_t>>
+        _sample_amplitudes; // in 0.1 dB (size = sum of _Number_of_samples of all Beams
+
+    uint8_t  _spare_byte = 0;
+    uint8_t  _etx        = 0x03; ///< end identifier (always 0x03)
+    uint16_t _checksum;
+
+  private:
+    // ----- private constructors -----
+    explicit SeabedImageData(EM3000Datagram header)
+        : EM3000Datagram(std::move(header))
+    {
+    }
+
+  public:
+    // ----- public constructors -----
+    SeabedImageData() { _datagram_identifier = t_EM3000DatagramIdentifier::SeabedImageData; }
+    ~SeabedImageData() = default;
+
+    // ----- convenient data access -----
+    uint16_t get_ping_counter() const { return _ping_counter; }
+    uint16_t get_system_serial_number() const { return _system_serial_number; }
+    float    get_sampling_frequency() const { return _sampling_frequency; }
+    uint16_t get_range_to_normal_incidence() const { return _range_to_normal_incidence; }
+    int16_t  get_normal_incidence_backscatter() const { return _normal_incidence_backscatter; }
+    int16_t  get_oblique_backscatter() const { return _oblique_backscatter; }
+    uint16_t get_tx_beamwidth_along() const { return _tx_beamwidth_along; }
+    uint16_t get_tvg_law_crossover_angle() const { return _tvg_law_crossover_angle; }
+    uint16_t get_number_of_valid_beams() const { return _number_of_valid_beams; }
+    uint8_t  get_spare_byte() const { return _spare_byte; }
+    uint8_t  get_etx() const { return _etx; }
+    uint16_t get_checksum() const { return _checksum; }
+
+    void set_ping_counter(uint16_t ping_counter) { _ping_counter = ping_counter; }
+    void set_system_serial_number(uint16_t system_serial_number)
+    {
+        _system_serial_number = system_serial_number;
+    }
+    void set_sampling_frequency(float sampling_frequency)
+    {
+        _sampling_frequency = sampling_frequency;
+    }
+    void set_range_to_normal_incidence(uint16_t range_to_normal_incidence)
+    {
+        _range_to_normal_incidence = range_to_normal_incidence;
+    }
+    void set_normal_incidence_backscatter(int16_t normal_incidence_backscatter)
+    {
+        _normal_incidence_backscatter = normal_incidence_backscatter;
+    }
+    void set_oblique_backscatter(int16_t oblique_backscatter)
+    {
+        _oblique_backscatter = oblique_backscatter;
+    }
+    void set_tx_beamwidth_along(uint16_t tx_beamwidth_along)
+    {
+        _tx_beamwidth_along = tx_beamwidth_along;
+    }
+    void set_tvg_law_crossover_angle(uint16_t tvg_law_crossover_angle)
+    {
+        _tvg_law_crossover_angle = tvg_law_crossover_angle;
+    }
+    void set_number_of_valid_beams(uint16_t number_of_valid_beams)
+    {
+        _number_of_valid_beams = number_of_valid_beams;
+    }
+    void set_spare_byte(uint8_t spare_byte) { _spare_byte = spare_byte; }
+    void set_etx(uint8_t etx) { _etx = etx; }
+    void set_checksum(uint16_t checksum) { _checksum = checksum; }
+
+    // ----- substructure access -----
+    const std::vector<substructures::SeabedImageDataBeam>& get_beams() const { return _beams; }
+    void set_beams(const std::vector<substructures::SeabedImageDataBeam>& beams) { _beams = beams; }
+    std::vector<substructures::SeabedImageDataBeam>& beams() { return _beams; }
+
+    const std::vector<std::vector<int16_t>>& get_sample_amplitudes() const
+    {
+        return _sample_amplitudes;
+    }
+    void set_sample_amplitudes(const std::vector<std::vector<int16_t>>& sample_amplitudes)
+    {
+        _sample_amplitudes = sample_amplitudes;
+    }
+    std::vector<std::vector<int16_t>>& sample_amplitudes() { return _sample_amplitudes; }
+
+    // ----- processed data access -----
+    /**
+     * @brief Get the normal incidence backscatter in db
+     *
+     * @return _normal_incidence_backscatter * 0.01f (float)
+     */
+    float get_normal_incidence_backscatter_in_db() const
+    {
+        return _normal_incidence_backscatter * 0.01f;
+    }
+
+    /**
+     * @brief Get the oblique backscatter in db
+     *
+     * @return _oblique_backscatter * 0.01f (float)
+     */
+    float get_oblique_backscatter_in_db() const { return _oblique_backscatter * 0.01f; }
+
+    /**
+     * @brief get the tx beamwidth along in degrees
+     *
+     * @return _tx_beamwidth_along * 0.1f (float)
+     */
+    float get_tx_beamwidth_along_in_degrees() const { return _tx_beamwidth_along * 0.1f; }
+
+    /**
+     * @brief get the tvg law crossover angle in degrees
+     *
+     * @return _tvg_law_crossover_angle * 0.1f (float)
+     */
+    float get_tvg_law_crossover_angle_in_degrees() const { return _tvg_law_crossover_angle * 0.1f; }
+
+    // ----- operators -----
+    bool operator==(const SeabedImageData& other) const
+    {
+        return _datagram_identifier == other._datagram_identifier &&
+               _ping_counter == other._ping_counter &&
+               _system_serial_number == other._system_serial_number &&
+               _sampling_frequency == other._sampling_frequency &&
+               _range_to_normal_incidence == other._range_to_normal_incidence &&
+               _normal_incidence_backscatter == other._normal_incidence_backscatter &&
+               _oblique_backscatter == other._oblique_backscatter &&
+               _tx_beamwidth_along == other._tx_beamwidth_along &&
+               _tvg_law_crossover_angle == other._tvg_law_crossover_angle &&
+               _number_of_valid_beams == other._number_of_valid_beams &&
+               _spare_byte == other._spare_byte && _etx == other._etx &&
+               _checksum == other._checksum && _beams == other._beams &&
+               _sample_amplitudes == other._sample_amplitudes;
+    }
+    bool operator!=(const SeabedImageData& other) const { return !operator==(other); }
+
+    //----- to/from stream functions -----
+    static SeabedImageData from_stream(std::istream& is, EM3000Datagram header)
+    {
+        SeabedImageData datagram(std::move(header));
+
+        if (datagram._datagram_identifier != t_EM3000DatagramIdentifier::SeabedImageData)
+            throw std::runtime_error(
+                fmt::format("SeabedImageData: datagram identifier is not 0x{:02x}, but 0x{:02x}",
+                            uint8_t(t_EM3000DatagramIdentifier::SeabedImageData),
+                            uint8_t(datagram._datagram_identifier)));
+
+        // read first part of the datagram (until the first beam)
+        is.read(reinterpret_cast<char*>(&(datagram._ping_counter)), 20 * sizeof(uint8_t));
+
+        // read the beams
+        datagram._beams.resize(datagram._number_of_valid_beams);
+        is.read(reinterpret_cast<char*>(datagram._beams.data()),
+                datagram._number_of_valid_beams * sizeof(substructures::SeabedImageDataBeam));
+
+        // read the sample amplitudes
+        datagram._sample_amplitudes.resize(datagram._number_of_valid_beams);
+        for (uint b = 0; b < datagram._number_of_valid_beams; ++b)
+        {
+            datagram._sample_amplitudes[b].resize(datagram._beams[b].get_number_of_samples());
+            is.read(reinterpret_cast<char*>(datagram._sample_amplitudes[b].data()),
+                    datagram._beams[b].get_number_of_samples() * sizeof(int16_t));
+        }
+
+        // read the rest of the datagram
+        is.read(reinterpret_cast<char*>(&(datagram._spare_byte)), 4 * sizeof(uint8_t));
+
+        if (datagram._etx != 0x03)
+            throw std::runtime_error(fmt::format(
+                "SeabedImageData: end identifier is not 0x03, but 0x{:x}", datagram._etx));
+
+        return datagram;
+    }
+
+    static SeabedImageData from_stream(std::istream& is)
+    {
+        return from_stream(is, EM3000Datagram::from_stream(is));
+    }
+
+    static SeabedImageData from_stream(std::istream&              is,
+                                       t_EM3000DatagramIdentifier datagram_identifier)
+    {
+        return from_stream(is, std::move(EM3000Datagram::from_stream(is, datagram_identifier)));
+    }
+
+    void to_stream(std::ostream& os)
+    {
+        EM3000Datagram::to_stream(os);
+        _number_of_valid_beams = _beams.size();
+
+        // write first part of the datagram (until the first beam)
+        os.write(reinterpret_cast<const char*>(&(_ping_counter)), 20 * sizeof(uint8_t));
+
+        // write the beams
+        os.write(reinterpret_cast<const char*>(_beams.data()),
+                 _number_of_valid_beams * sizeof(substructures::SeabedImageDataBeam));
+
+        // write the sample amplitudes
+        _sample_amplitudes.resize(_number_of_valid_beams);
+        for (uint b = 0; b < _number_of_valid_beams; ++b)
+        {
+            _sample_amplitudes[b].resize(_beams[b].get_number_of_samples());
+            os.write(reinterpret_cast<const char*>(_sample_amplitudes[b].data()),
+                     _beams[b].get_number_of_samples() * sizeof(int16_t));
+        }
+
+        // write the rest of the datagram
+        os.write(reinterpret_cast<const char*>(&(_spare_byte)), 4 * sizeof(uint8_t));
+    }
+
+    // ----- objectprinter -----
+    tools::classhelper::ObjectPrinter __printer__(unsigned int float_precision) const
+    {
+        tools::classhelper::ObjectPrinter printer("SeabedImageData", float_precision);
+
+        printer.append(EM3000Datagram::__printer__(float_precision));
+        printer.register_section("datagram content");
+        printer.register_value("ping_counter", _ping_counter);
+        printer.register_value("system_serial_number", _system_serial_number);
+        printer.register_value("sampling_frequency", _sampling_frequency, "Hz");
+        printer.register_value("range_to_normal_incidence", _range_to_normal_incidence);
+        printer.register_value("normal_incidence_backscatter", _normal_incidence_backscatter, "0.01 dB");
+        printer.register_value("oblique_backscatter", _oblique_backscatter, "0.01 dB");
+        printer.register_value("tx_beamwidth_along", _tx_beamwidth_along, "0.1°");
+        printer.register_value("tvg_law_crossover_angle", _tvg_law_crossover_angle, "0.1°");
+        printer.register_value("number_of_valid_beams", _number_of_valid_beams);
+        printer.register_value("spare_byte", _spare_byte);
+        printer.register_string("etx", fmt::format("0x{:02x}", _etx));
+        printer.register_value("checksum", _checksum);
+
+        printer.register_section("processed");
+        printer.register_value("normal_incidence_backscatter", get_normal_incidence_backscatter_in_db(), "dB");
+        printer.register_value("oblique_backscatter", get_oblique_backscatter_in_db(), "dB");
+        printer.register_value("tx_beamwidth_along", get_tx_beamwidth_along_in_degrees(), "°");
+        printer.register_value("tvg_law_crossover_angle", get_tvg_law_crossover_angle_in_degrees(), "°");
+
+        printer.register_section("substructures");
+        printer.register_value("SeabedImageDataBeams", _beams.size(), "structures");
+        printer.register_value("sample_amplitudes", _sample_amplitudes.size(), "beams");
+
+        return printer;
+    }
+
+    // ----- class helper macros -----
+    __CLASSHELPER_DEFAULT_PRINTING_FUNCTIONS__
+    __STREAM_DEFAULT_TOFROM_BINARY_FUNCTIONS_NOT_CONST__(SeabedImageData)
+};
+
+} // namespace datagrams
+} // namespace em3000
+} // namespace echosounders
+} // namespace themachinethatgoesping
---
+++ src/themachinethatgoesping/echosounders/em3000/datagrams/substructures/seabedimagedatabeam.hpp
@@ -0,0 +1,135 @@
+// SPDX-FileCopyrightText: 2022 Peter Urban, GEOMAR Helmholtz Centre for Ocean Research Kiel
+// SPDX-FileCopyrightText: 2022 Sven Schorge, GEOMAR Helmholtz Centre for Ocean Research Kiel
+// SPDX-FileCopyrightText: 2022 - 2023 Peter Urban, Ghent University
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+
+/* generated doc strings */
+#include ".docstrings/seabedimagedatabeam.doc.hpp"
+
+// std includes
+#include <string>
+
+// themachinethatgoesping import
+#include <themachinethatgoesping/tools/classhelper/objectprinter.hpp>
+#include <themachinethatgoesping/tools/classhelper/stream.hpp>
+#include <themachinethatgoesping/tools/timeconv.hpp>
+
+#include "../../em3000_types.hpp"
+
+#include "flag_detection_information.hpp"
+
+namespace themachinethatgoesping {
+namespace echosounders {
+namespace em3000 {
+namespace datagrams {
+namespace substructures {
+
+/**
+ * @brief The beam data are given re the transmit transducer or sonar head depth and the horizontal
+ * location (x,y) of the active positioning system's reference point. Heave, roll, pitch, sound
+ * speed at the transducer depth and ray bending through the water column have been applied.
+ *
+ */
+class SeabedImageDataBeam
+{
+    int8_t   _sorting_direction;
+    uint8_t  _detection_info;
+    uint16_t _number_of_samples; // per beam
+    uint16_t _centre_sample_number;
+
+  public:
+    SeabedImageDataBeam()  = default;
+    ~SeabedImageDataBeam() = default;
+
+    // ----- convenient member access -----
+    // get 
+    uint16_t get_sorting_direction() const { return _sorting_direction; }
+    uint8_t  get_detection_info() const { return _detection_info; }
+    uint16_t get_number_of_samples() const { return _number_of_samples; }
+    uint16_t get_centre_sample_number() const { return _centre_sample_number; }
+
+    // set
+    void set_sorting_direction(const uint16_t& sorting_direction) { _sorting_direction = sorting_direction; }
+    void set_detection_info(const uint8_t& detection_info) { _detection_info = detection_info; }
+    void set_number_of_samples(const uint16_t& number_of_samples) { _number_of_samples = number_of_samples; }
+    void set_centre_sample_number(const uint16_t& centre_sample_number) { _centre_sample_number = centre_sample_number; }
+
+    // ----- processed member access -----
+    /**
+     * @brief This function evaluates the detection information flag. If the last bit is set to 1,
+     * the detection is valid. If the last bit is set to 0, the detection is invalid.
+     *
+     * @return true
+     * @return false
+     */
+    bool get_detection_is_valid() const
+    {
+        return detection_information::get_detection_is_valid(_detection_info);
+    }
+
+    /**
+     * @brief This function evaluates the detection information flag. The first 3 bits indicate the
+     * type of detection.
+     *
+     * @return t_DetectionType
+     */
+    detection_information::t_DetectionType get_detection_type() const
+    {
+        return detection_information::get_detection_type(_detection_info);
+    }
+
+    /**
+     * @brief This function evaluates the detection information flag. If the 4th bit is set to 1,
+     * the detection is compensated for beam incident angle. If the 4th bit is set to 0, the
+     * detection is not compensated for beam incident angle.
+     *
+     * @return true
+     * @return false
+     */
+    bool get_backscatter_is_compensated() const
+    {
+        return detection_information::get_backscatter_is_compensated(_detection_info);
+    }
+
+    // ----- operators -----
+    bool operator==(const SeabedImageDataBeam& other) const
+    {
+        return _sorting_direction == other._sorting_direction &&
+               _detection_info == other._detection_info &&
+               _number_of_samples == other._number_of_samples &&
+               _centre_sample_number == other._centre_sample_number;
+    }
+    bool operator!=(const SeabedImageDataBeam& other) const { return !(*this == other); }
+
+    // ----- objectprinter -----
+    tools::classhelper::ObjectPrinter __printer__(unsigned int float_precision) const
+    {
+        tools::classhelper::ObjectPrinter printer("SeabedImageDataBeam", float_precision);
+
+        // raw values
+        printer.register_value("sorting_direction", _sorting_direction);
+        printer.register_value("detection_info", _detection_info);
+        printer.register_value("number_of_samples", _number_of_samples, "per beam");
+        printer.register_value("centre_sample_number", _centre_sample_number);
+
+        // processed values
+        printer.register_section("processed");
+        printer.register_value("detection_is_valid", get_detection_is_valid());
+        printer.register_enum("detection_type", get_detection_type());
+        printer.register_value("backscatter_is_compensated", get_backscatter_is_compensated());
+
+        return printer;
+    }
+
+    // ----- class helper macros -----
+    __CLASSHELPER_DEFAULT_PRINTING_FUNCTIONS__
+};
+
+} // namespace substructures
+} // namespace datagrams
+} // namespace em3000
+} // namespace echosounders
+} // namespace themachinethatgoesping
