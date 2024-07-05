6872f4e410119bba31394c766253cb42e210c765
Peter Urban <peter.urban@ugent.be>
2024-07-05 10:36:34 +0200

    include tests opening files and looping through pings and datagrams

---
+++ python/tests/kongsbergall/test_kongsbergallfilehandler.py
@@ -0,0 +1,75 @@
+# SPDX-FileCopyrightText: 2022 - 2023 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: MPL-2.0
+
+# ignore pylint warning protected-access
+# pylint: disable=protected-access
+# pylint: disable=no-self-use
+
+import os
+import logging
+import themachinethatgoesping as Ping
+from themachinethatgoesping.echosounders import kongsbergall
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Test_echosounders_kongsbergall_KongsbergAllFileHandler:
+    files = None
+
+    def find_files(self):
+        if self.files is not None:
+            return
+
+        dirname = os.path.dirname(__file__)
+        self.test_folders = os.path.join(dirname, "../../../unittest_data/")
+
+        LOGGER.info(f"findings test files in {self.test_folders}")
+        self.files_all = Ping.echosounders.index_functions.find_files(self.test_folders, ".all")
+        self.files_wcd = Ping.echosounders.index_functions.find_files(self.test_folders, ".wcd")
+        self.files = self.files_all + self.files_wcd
+        self.files.sort()
+        self.cache_files = Ping.echosounders.index_functions.get_cache_file_paths(self.files)
+
+        assert len(self.files) > 0
+        assert len(self.files_all) > 0
+        assert len(self.files_wcd) > 0
+        assert len(self.cache_files) > 0
+
+    def clean_cache(self):
+        LOGGER.info(f"Cleaning up cache files")
+        for file in self.cache_files:
+            os.remove(file)
+
+    def open_files_and_loop_through_pings(self, file, cache=False):
+        if cache:
+            fm = kongsbergall.KongsbergAllFileHandler(file, file_cache_paths=self.cache_files, show_progress=False)
+        else:
+            fm = kongsbergall.KongsbergAllFileHandler(file, show_progress=False)
+
+        for d in fm.datagram_interface.datagrams():
+            d.get_timestamp()
+
+        for ping in fm.get_pings():
+            ping.get_timestamp()
+
+        return fm.get_pings()
+
+    def test_open_files_should_not_crash(self):
+        self.find_files()
+
+        # individually open all files and loop through all pings
+        for file in self.files:
+            LOGGER.info(f"Opening {file}")
+            assert len(self.open_files_and_loop_through_pings(file, cache=False)) > 0
+            assert len(self.open_files_and_loop_through_pings(file, cache=True)) > 0
+            assert len(self.open_files_and_loop_through_pings(file, cache=True)) > 0
+            assert len(self.open_files_and_loop_through_pings(file, cache=False)) > 0
+
+        for files in [self.files_all, self.files_wcd, self.files]:
+            # open all files at once and loop through all pings
+            assert len(self.open_files_and_loop_through_pings(files, cache=False)) > 0
+            assert len(self.open_files_and_loop_through_pings(files, cache=True)) > 0
+            assert len(self.open_files_and_loop_through_pings(files, cache=True)) > 0
+            assert len(self.open_files_and_loop_through_pings(files, cache=False)) > 0
---
+++ python/tests/simradraw/test_simradrawfilehandler.py
@@ -0,0 +1,70 @@
+# SPDX-FileCopyrightText: 2024 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: MPL-2.0
+
+# ignore pylint warning protected-access
+# pylint: disable=protected-access
+# pylint: disable=no-self-use
+
+import os
+import logging
+import themachinethatgoesping as Ping
+from themachinethatgoesping.echosounders import simradraw
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Test_echosounders_simradraw_SimradRawFileHandler:
+    files = None
+
+    def find_files(self):
+        if self.files is not None:
+            return
+
+        dirname = os.path.dirname(__file__)
+        self.test_folders = os.path.join(dirname, "../../../unittest_data/")
+
+        LOGGER.info(f"findings test files in {self.test_folders}")
+        self.files = Ping.echosounders.index_functions.find_files(self.test_folders, ".raw")
+        self.files.sort()
+        self.cache_files = Ping.echosounders.index_functions.get_cache_file_paths(self.files)
+
+        assert len(self.files) > 0
+        assert len(self.cache_files) > 0
+
+    def clean_cache(self):
+        LOGGER.info(f"Cleaning up cache files")
+        for file in self.cache_files:
+            os.remove(file)
+
+    def open_files_and_loop_through_pings(self, file, cache=False):
+        if cache:
+            fm = simradraw.SimradRawFileHandler(file, file_cache_paths=self.cache_files, show_progress=False)
+        else:
+            fm = simradraw.SimradRawFileHandler(file, show_progress=False)
+
+        for d in fm.datagram_interface.datagrams():
+            d.get_timestamp()
+
+        for ping in fm.get_pings():
+            ping.get_timestamp()
+
+        return fm.get_pings()
+
+    def test_open_files_should_not_crash(self):
+        self.find_files()
+
+        # individually open all files and loop through all pings
+        for file in self.files:
+            LOGGER.info(f"Opening {file}")
+            assert len(self.open_files_and_loop_through_pings(file, cache=False)) > 0
+            assert len(self.open_files_and_loop_through_pings(file, cache=True)) > 0
+            assert len(self.open_files_and_loop_through_pings(file, cache=True)) > 0
+            assert len(self.open_files_and_loop_through_pings(file, cache=False)) > 0
+
+        # open all files at once and loop through all pings
+        assert len(self.open_files_and_loop_through_pings(self.files, cache=False)) > 0
+        assert len(self.open_files_and_loop_through_pings(self.files, cache=True)) > 0
+        assert len(self.open_files_and_loop_through_pings(self.files, cache=True)) > 0
+        assert len(self.open_files_and_loop_through_pings(self.files, cache=False)) > 0
