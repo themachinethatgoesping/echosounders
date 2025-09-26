a2f643e20f87fe8c7c6302376626da66a3d4066b
Peter Urban <peter.urban@ugent.be>
2025-09-26 11:38:07 +0200

    expose find_files

--- python/themachinethatgoesping/echosounders/__init__.py
+++ python/themachinethatgoesping/echosounders/__init__.py
@@ -9,7 +9,7 @@ from pkgutil import extend_path as __extend_path__
 from themachinethatgoesping.echosounders_cppy import *  # flake8: noqa
 
 # python only modules
-from . index_functions import get_index_paths # flake8: noqa
+from . index_functions import get_index_paths, find_files # flake8: noqa
 from .evaluate.evaluate_ping_features import evaluate_ping_features_can_be_called # flake8: noqa
 
 __version__ = "@PROJECT_VERSION@"
