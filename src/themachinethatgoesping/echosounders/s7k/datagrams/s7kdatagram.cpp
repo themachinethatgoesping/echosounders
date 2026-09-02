7519f30d88d2fc055fa75abd28d0e6b8958f6222
Peter Urban <peter.urban@ugent.be>
2026-09-02 20:18:01 +0200

    restructing s7k datagramheader

--- src/themachinethatgoesping/echosounders/s7k/datagrams/s7kdatagram.cpp
+++ src/themachinethatgoesping/echosounders/s7k/datagrams/s7kdatagram.cpp
@@ -22,7 +22,7 @@ S7KDatagram S7KDatagram::from_stream(std::istream& is)
     return d;
 }
 
-S7KDatagram S7KDatagram::from_stream(std::istream&           is,
+S7KDatagram S7KDatagram::from_stream(std::istream&                 is,
                                      const o_S7KDatagramIdentifier datagram_identifier)
 {
     S7KDatagram d = from_stream(is);
@@ -37,25 +37,34 @@ void S7KDatagram::to_stream(std::ostream& os) const
 }
 
 tools::classhelper::ObjectPrinter S7KDatagram::__printer__(unsigned int float_precision,
-                                                           bool superscript_exponents) const
+                                                           bool         superscript_exponents) const
 {
     tools::classhelper::ObjectPrinter printer(
         "S7KDatagram", float_precision, superscript_exponents);
 
     printer.register_value("protocol_version", _protocol_version);
+    printer.register_value("offset", _offset);
+    printer.register_value("sync_pattern", fmt::format("0x{:08X}", _sync_pattern));
     printer.register_value("size", _size, "bytes");
+
+    printer.register_value("optional_data_offset", _optional_data_offset, "bytes");
+    printer.register_value("optional_data_identifier", _optional_data_identifier);
+
+    printer.register_value("record_version", _record_version);
     printer.register_value("record_type_identifier",
                            uint32_t(_record_type_identifier),
                            datagram_type_to_string(_record_type_identifier));
     printer.register_value("device_identifier", _device_identifier);
     printer.register_value("system_enumerator", _system_enumerator);
-    printer.register_value("record_version", _record_version);
-    printer.register_value("optional_data_offset", _optional_data_offset, "bytes");
-    printer.register_value("optional_data_identifier", _optional_data_identifier);
-    printer.register_value("flags", _flags);
-    printer.register_value("sync_pattern", fmt::format("0x{:08X}", _sync_pattern));
+    printer.register_value_bytes("flags", _flags);
+    printer.register_value("total_records_in_fragmented_data_record_set",
+                           _total_records_in_fragmented_data_record_set);
+    printer.register_value("fragment_number", _fragment_number);
 
     // time processing (if time is provided)
+    // printer.register_container("year, day, seconds, hours, minutes",
+    //                            std::make_tuple<uint16_t, uint16_t, float, uint8_t, uint8_t>(
+    //                                _year, _day, _seconds, _hours, _minutes));
     auto timestamp = get_timestamp();
     if (std::isfinite(timestamp))
     {
@@ -70,6 +79,7 @@ tools::classhelper::ObjectPrinter S7KDatagram::__printer__(unsigned int float_pr
         printer.register_string("date", date, "DD/MM/YYYY");
         printer.register_string("time", time, "HH:MM:SS");
     }
+
 
     return printer;
 }
